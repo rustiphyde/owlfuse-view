329f0813b60e3d6a03614a150632ae26b8692c3e
rustiphyde <rustiphyde@gmail.com>
2019-09-26 00:18:48 -0500

    Create components folder with Navbar and utilize react router dom in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+
+// Pages
+import home from './pages/home';
+import login from './pages/login';
+import signup from './pages/signup';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={home}/>
+          <Route exact path="/login" component={login}/>
+          <Route exact path="/signup" component={signup}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
