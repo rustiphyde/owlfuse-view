85e026d265104d305d765be1d6fdda891d14f143
rustiphyde <rustiphyde@gmail.com>
2019-09-26 01:34:55 -0500

    create icons folder for custom icons and redux folder with types file including every type designed so far

---
+++ src/redux/types.js
@@ -0,0 +1,42 @@
+// User Reducer Types
+export const SET_AUTHENTICATED = 'SET_AUTHENTICATED';
+export const SET_UNAUTHENTICATED = 'SET_UNAUTHENTICATED';
+export const SET_USER = 'SET_USER';
+export const LOADING_USER = 'LOADING_USER';
+export const MARK_SIZZLES_READ = 'MARK_SIZZLES_READ';
+// UI Reducer Types
+export const SET_ERRORS = 'SET_ERRORS';
+export const LOADING_UI = 'LOADING_UI';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+export const SET_SUCCESS = 'SET_SUCCESS';
+export const CLEAR_SUCCESS = 'CLEAR_SUCCESS';
+// Data Reducer Types
+export const SET_FIRES = 'SET_FIRES';
+export const SET_FIRE = 'SET_FIRE';
+export const SET_SPARKS = 'SET_SPARKS';
+export const SET_SPARK = 'SET_SPARK';
+export const BURN_SPARK = 'BURN_SPARK';
+export const SNUFF_SPARK = 'SNUFF_SPARK';
+export const EXTINGUISH_SPARK = 'EXTINGUISH_SPARK';
+export const LIGHT_SPARK = 'LIGHT_SPARK';
+export const LOADING_DATA = 'LOADING_DATA';
+export const ADD_STOKE = 'ADD_STOKE';
+export const ADD_EMBER = 'ADD_EMBER';
+export const ADD_HEAT = 'ADD_HEAT';
+export const REMOVE_HEAT = 'REMOVE_HEAT';
+export const SET_OKES = 'SET_OKES';
+export const SET_BOOZULAS = 'SET_BOOZULAS';
+export const SET_OKE = 'SET_OKE';
+export const SET_BOOZULA = 'SET_BOOZULA';
+export const EXTINGUISH_FIRE = 'EXTINGUISH_FIRE';
+export const ADD_CHEERS = 'ADD_CHEERS';
+export const UNCHEERS = 'UNCHEERS';
+export const EMPTY_BOOZULA = 'EMPTY_BOOZULA';
+export const CHANGE_BOOZ_IMAGE = 'CHANGE_BOOZ_IMAGE';
+export const BUILD_BOOZULA = 'BUILD_BOOZULA';
+export const BUILD_OKE = 'BUILD_OKE';
+export const ERASE_OKE = 'ERASE_OKE';
+export const ADD_SONG = 'ADD_SONG';
+export const ADD_TOAST = 'ADD_TOAST';
+export const CHOOZ_BY_LIST = 'CHOOZ_BY_LIST';
