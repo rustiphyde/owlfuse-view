33231178b2ba3a28581e3dc7898788c92c42e7a9
rustiphyde <rustiphyde@gmail.com>
2019-09-26 17:17:54 -0500

    Create helper function for setting the authorization header

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,35 +1,58 @@
 import { SET_ERRORS, SET_USER, CLEAR_ERRORS, LOADING_UI } from "../types";
-import axios from 'axios';
+import axios from "axios";
 
 export const loginUser = (userData, history) => dispatch => {
   dispatch({ type: LOADING_UI });
   axios
     .post("/login", userData)
     .then(res => {
-      const FBIdToken = `Bearer ${res.data.token}`
-      localStorage.setItem("FBIdToken", FBIdToken);
-      axios.defaults.headers.common['Authorization'] = FBIdToken;
-      dispatch(getAuthenticatedUser());
-      dispatchEvent({ type: CLEAR_ERRORS })
-      history.push('/');
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
     })
     .catch(err => {
       dispatch({
         type: SET_ERRORS,
         payload: err.response.data
-      })
+      });
     });
 };
 
-// action creator for fetching an authenticated user's data
-export const getAuthenticatedUser = () => dispatch => {
+export const signupUser = (newUserData, history) => dispatch => {
+    dispatch({ type: LOADING_UI });
     axios
-      .get("/user")
+      .post("/signup", newUserData)
       .then(res => {
-        dispatch({
-          type: SET_USER,
-          payload: res.data
-        });
+        setAuthorizationHeader(res.data.token);
+        dispatch(getUserData());
+        dispatch({ type: CLEAR_ERRORS });
+        history.push("/");
       })
-      .catch(err => console.log(err));
+      .catch(err => {
+        dispatch({
+          type: SET_ERRORS,
+          payload: err.response.data
+        });
+      });
+  };
+  
+
+export const getUserData = () => dispatch => {
+  axios
+    .get("/user")
+    .then(res => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+// Helper fxn for setting authorization header in various places
+const setAuthorizationHeader = token => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem("FBIdToken", FBIdToken);
+    axios.defaults.headers.common["Authorization"] = FBIdToken;
   };
