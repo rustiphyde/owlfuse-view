0e5a6a1d532d1a63e84e65b42613451287c1eed0
rustiphyde <rustiphyde@gmail.com>
2019-09-26 12:03:43 -0500

    Add styles and component to sign up page with similar template as login

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,13 +1,158 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import AppIcon from "../images/owlfuse-logo-big.png";
+import { Link } from "react-router-dom";
+import axios from "axios";
+// MUI Stuff
+import withStyles from "@material-ui/core/styles/withStyles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const styles = theme => ({
+  ...theme.themeMinusPalette
+});
 
 class signup extends Component {
-    render() {
-        return (
-            <div>
-               <h1>Sign Up Page</h1> 
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      alias: "",
+      loading: false,
+      errors: {}
+    };
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      loading: true
+    });
+    // Save input to userData variable
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword,
+      alias: this.state.alias
+    };
+    axios
+      .post("/signup", userData)
+      .then(res => {
+        this.setState({
+          loading: false
+        });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        this.setState({
+          errors: err.response.data,
+          loading: false
+        });
+      });
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img
+            src={AppIcon}
+            alt="OwlFuse Logo"
+            className={classes.image}
+            width="120"
+          />
+          <Typography variant="h4" className={classes.pageTitle}>
+            <strong>SIGN UP</strong>
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="EMAIL ADDRESS"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="PASSWORD"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="CONFIRM PASSWORD"
+              className={classes.textField}
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="alias"
+              name="alias"
+              type="name"
+              label="ALIAS"
+              className={classes.textField}
+              helperText={errors.alias}
+              error={errors.alias ? true : false}
+              value={this.state.alias}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              disabled={loading}
+            >
+              <strong>SIGN UP</strong>
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              HAVE AN ACCOUNT? LOG IN <Link to="/login">HERE</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
+    );
+  }
 }
-
-export default signup
+signup.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(signup);
