c4fecdd61b29887913e1352a4eb64a1ea84fb4e2
rustiphyde <rustiphyde@gmail.com>
2019-09-27 00:16:02 -0500

    Add Profile Mockup to display details

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,81 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import { logoutUser } from '../redux/actions/userActions';
 import withStyles from "@material-ui/core/styles/withStyles";
 // MUI Stuff
 import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 // Icons
+import LocationIcon from './icons/LocationIcon';
 // Redux Stuff
 import { connect } from "react-redux";
 
-const styles = {
-
-}
+const styles = theme => ({
+  paper: {
+    padding: 16,
+    borderRadius: "16px 0 16px 0",
+    borderTop: "2px solid #ff9800",
+    borderBottom: "2px solid #ff9800",
+    borderLeft: "2px solid #ff9800",
+    backgroundColor: "#37474f"
+  },
+  profile: {
+    margin: 8,
+    borderTop: "2px solid #ff9800",
+    borderBottom: "2px solid #ff9800",
+    borderRight: "2px solid #ff9800",
+    backgroundColor: "#fff",
+    borderRadius: "16px 0 16px 0",
+    "& .image-wrapper": {
+      textAlign: "center",
+      position: "relative",
+      "& button": {
+        position: "absolute",
+        top: "80%",
+        left: "70%",
+        padding: "16px"
+      }
+    },
+    "& .profile-image": {
+      border: "2px solid #ff9800",
+      width: 160,
+      height: 160,
+      objectFit: "cover",
+      maxWidth: "100%",
+      borderRadius: "50%",
+      margin: 8
+    },
+    "& .profile-details": {
+      margin: 8,
+      textAlign: "center",
+      "& span, svg": {
+        verticalAlign: "middle"
+      }
+    },
+    "& hr": {
+      border: "none",
+      margin: "0 0 10px 0"
+    },
+    "& svg.button": {
+      "&:hover": {
+        cursor: "pointer"
+      }
+    }
+  },
+  buttons: {
+    textAlign: "center",
+    "& a": {
+      margin: "20px 10px"
+    }
+  },
+  joined: {
+    color: "#ff9800"
+  }
+});
 
 class Profile extends Component {
   render() {
@@ -17,10 +83,68 @@ class Profile extends Component {
       classes,
       user: {
         credentials: { alias, clozang, createdAt, imageUrl, bio, website, location },
-        loading
+        loading,
+        authenticated
       }
     } = this.props;
-    return <div />;
+
+    let profileMarkup = !loading ? (authenticated ? (
+      <Paper className={classes.paper}>
+        <div className={classes.profile}>
+          <div className="image-wrapper">
+            <img src={imageUrl} alt="profile" className="profile-image" />
+          </div>
+          <hr />
+          <div className="profile-details">
+            <MuiLink component={Link} to={`/users/${clozang}`} color="primary" variant="h5">
+              <strong>>{alias}</strong>
+            </MuiLink>
+            <hr />
+            {bio && <Typography variant="body2">{bio}</Typography>}
+            <hr />
+            {location && (
+              <Fragment>
+                <LocationIcon color="secondary" /> <span>{location}</span>
+                <hr />
+              </Fragment>
+            )}
+            {website && (
+              <Fragment>
+                <a href={website} target="_blank" rel="noopener noreferrer">
+                  {' '}{website}
+                </a>
+                <hr />
+              </Fragment>
+            )}
+                    
+            <span>Member since {dayjs(createdAt).format('MMM YYYY')}</span>
+          </div>
+        </div>
+      </Paper>
+    ) : (
+        <Paper className={classes.paper}>
+          <Typography variant="body2" align="center">
+            NO PROFILE FOUND, PLEASE LOGIN AGAIN OR SIGN UP FOR A NEW ACCOUNT
+                </Typography>
+          <div className={classes.buttons}>
+            <Button variant="contained" color="primary"
+              component={Link} to="/login">
+              LOGIN
+                    </Button>
+            <Button variant="contained" color="secondary"
+              component={Link} to="/signup">
+              SIGN UP
+                    </Button>
+          </div>
+        </Paper>
+      )
+    ) : (
+        <p className="loading">
+    <strong>Loading...</strong>
+  </p>
+      );
+
+    return profileMarkup;
   }
 }
 
---
+++ src/components/icons/LocationIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const LocationIcon = (props) => (
+  <SvgIcon {...props}>
+  <path d="M8.1,11.8l-0.8,1.6C7.3,13.6,5.6,5.2,9,2.7c2.9-2,7.5-0.6,7.7-0.8l1-1.8c0,0,1.5,8.2-1.6,10.8c-1,0.7-3.8,12.4-3.8,12.4
+		s-2.1-11.3-3-11.4C8.6,11.9,8.1,11.8,8.1,11.8z M15.1,4.1l-0.8,0.8c0,0-3.1-0.4-4.1,0.7S9.5,9.9,9.5,9.9l0.8-1c0,0,2.8,0.6,3.9-0.4
+		S15.1,4.1,15.1,4.1z"/>
+  </SvgIcon>
+)
+
+export default LocationIcon;
