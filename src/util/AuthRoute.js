d356382792fa78f25f30a1e8cfb91064cfa3b019
rustiphyde <rustiphyde@gmail.com>
2019-09-26 23:18:09 -0500

    Use connect to handle authenticated state in AuthRoute.js

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 const AuthRoute = ({ component: Component, authenticated, ...rest }) => (
-    <Route{...rest}
-    render={(props) => authenticated === true ? <Redirect to="/"/> : <Component {...props}/>}
-    />
-    )
+  <Route
+    {...rest}
+    render={props =>
+      authenticated === true ? <Redirect to="/" /> : <Component {...props} />
+    }
+  />
+);
 
-    export default AuthRoute
+const mapStateToProps = state => ({
+  authenticated: state.user.authenticated
+});
+
+AuthRoute.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(AuthRoute);
