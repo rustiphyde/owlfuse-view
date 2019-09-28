498c25b0a754eebf07d1d6a63ba7338ab9b4e304
rustiphyde <rustiphyde@gmail.com>
2019-09-28 15:33:37 -0500

    Create icons for Okelists and Boozulas

---
+++ src/components/icons/BoozulaIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const BoozulaIcon = (props) => (
+    <SvgIcon {...props}>
+	<path d="M16.8,21.9c-3.2,0.7-6.4,0.7-9.6,0C6.3,21.7,5.6,21,5.4,20C4.5,16.1,2,5.6,1.4,2.8C1.3,2.5,1.6,2.2,2,2.2h20.2
+	c0.3,0,0.6,0.3,0.5,0.7c-0.6,2.7-3.1,13.3-4,17.2C18.5,21,17.7,21.7,16.8,21.9z M20.8,3.7H3.2l1.4,5.7c4.9-0.9,9.8-0.9,14.7,0
+	L20.8,3.7z"/>
+    </SvgIcon>
+)
+
+export default BoozulaIcon;
