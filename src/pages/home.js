5d0ff4222fd55061a552afee290fd27732f8bfaa
rustiphyde <rustiphyde@gmail.com>
2019-09-28 17:07:58 -0500

    Use connect to implement data actions on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import AppIcon from "../images/owlfuse-logo-big.png";
+import PropTypes from 'prop-types';
 // Components
 import Spark from '../components/sparks/Spark';
 import Candle from '../components/Candle';
@@ -11,33 +11,31 @@ import FlameIcon from '../components/icons/FlameIcon';
 // MUI Components
 import Grid from '@material-ui/core/Grid';
 
+// Redux
+import { connect } from 'react-redux';
+import { getSparks } from '../redux/actions/dataActions';
+
 class home extends Component {
   // Initialize Component State for storing the Sparks
   state = {
     sparks: null
   };
   componentDidMount() {
-    axios
-      .get("/sparks")
-      .then(res => {
-        this.setState({
-          sparks: res.data
-        });
-      })
-      .catch(err => console.log(err));
+    this.props.getSparks();
   }
   render() {
-    let recentSparksMarkup = this.state.sparks ? (
-      this.state.sparks.map(spark => <Spark key={spark.sparkId} spark={spark}/>)
+    const { sparks, loading } = this.props.data; 
+    let recentSparksMarkup = !loading ? (
+      sparks.map(spark => <Spark key={spark.sparkId} spark={spark} />)
     ) : (
-      <p>Loading...</p>
-      );
+      <p className="loading"><strong>Loading...</strong></p>
+    );
     return (
-      <Grid container spacing={2}>
-        <Grid item sm={8} xs={12}>
+      <Grid container spacing={2}>        
         
+        <Grid item sm={8} xs={12}>
           <div className="sparkTitle">
-            <strong>SPARKS</strong>
+            <strong><em>SPARKS</em></strong>
             <hr className="bar-separator"/>
           </div>
           {recentSparksMarkup}
@@ -58,4 +56,14 @@ class home extends Component {
   }
 }
 
-export default home;
+home.propTypes = {
+  getSparks: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  data: state.data
+})
+
+
+export default connect(mapStateToProps, { getSparks })(home);
