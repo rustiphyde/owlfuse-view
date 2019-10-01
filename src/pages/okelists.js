d5c22c125409847c75f76b2bbd6933ee26181179
rustiphyde <rustiphyde@gmail.com>
2019-10-01 02:04:14 -0500

    Add structure to okelists page including new PostOkelist component

--- src/pages/okelists.js
+++ src/pages/okelists.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getOkelists } from '../redux/actions/dataActions';
 
-export class okelists extends Component {
+// MUI Stuff
+import Grid from "@material-ui/core/Grid";
+// Components
+import Okelist from "../components/okelists/Okelist";
+import PostOkelist from '../components/okelists/PostOkelist';
+
+class okelists extends Component {
+  // Initialize Component State for storing the Sparks
+  state = {
+    okelists: null
+  };
+  componentDidMount() {
+    this.props.getOkelists();
+  }
   render() {
+    const { okelists, loading } = this.props.data
+    let recentOkelistsMarkup = !loading ? (
+      okelists.map(okelist => <Okelist key={okelist.okeId} okelist={okelist} />)
+    ) : (
+      <p className="loading"><strong>Loading...</strong></p>
+    );
     return (
-      <div>
-        
-      </div>
-    )
+      <Grid container spacing={2} className="sheet">
+      <Grid item sm={2}/>
+        <Grid item sm={8} xs={12}>
+        <div className="okeTitle">
+          <strong><em>OKE LISTS</em></strong>
+          <hr className="bar-separator-oke"/>
+          <PostOkelist/>
+          <hr className="bar-separator-oke"/>
+          </div>
+          {recentOkelistsMarkup}
+        </Grid>
+        <Grid item sm={2}/>
+      </Grid>
+    );
   }
 }
 
-export default okelists
+okelists.propTypes = {
+  getOkelists: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  data: state.data
+})
+
+export default connect(mapStateToProps, { getOkelists })(okelists);
