50995af3c14e12778277b94a6597aaea5a6d31df
rustiphyde <rustiphyde@gmail.com>
2019-10-28 00:41:41 -0500

    Add new folder and component for Infernals

---
+++ src/components/infernals/Infernal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Infernal extends Component {
+  render() {
+    return (
+      <div>
+        Infernals go here
+      </div>
+    )
+  }
+}
+
+export default Infernal
