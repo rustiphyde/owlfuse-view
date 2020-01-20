6e94d96470db4c4758636c38cad023beea7eb483
rustiphyde <rustiphyde@gmail.com>
2020-01-20 00:28:03 -0600

    Update Navbar Icons to reflect new direction and create new Howl icons

---
+++ src/components/icons/OwlFuseLogo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const OwlFuseLogo = props => (
+    <SvgIcon {...props}>
+        <path d="M18.2,19.1c5.2-4.4,2.7-18.5,2.7-18.5l-1.5,3.1C19,4.1,11.1,1.8,6.3,5.2C0.5,9.5,3.6,23.8,3.4,23.4l1.4-2.7
+	C4.7,20.7,13.6,22.4,18.2,19.1z M15,15.1c-1.7,1.7-6.6,0.8-6.6,0.8l-1.3,1.5c0,0-0.4-5.4,1.4-7.3s6.9-1.2,6.9-1.2l1.4-1.3
+	C16.7,7.4,16.9,13.1,15,15.1z"/>
+    </SvgIcon>
+)
+
+export default OwlFuseLogo;
