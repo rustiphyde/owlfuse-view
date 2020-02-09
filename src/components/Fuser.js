13a0dee37513f8cff999c431e08af8d5ed2b4b6d
rustiphyde <rustiphyde@gmail.com>
2020-02-09 00:53:37 -0600

    Create new component for displaying each Fuser in the FuseList

---
+++ src/components/Fuser.js
@@ -0,0 +1,49 @@
+import React, { Fragment, Component } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+// MUI Stuff
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
+
+
+const styles = {
+    paper: {
+        backgroundColor: "#263238",
+        color: "#f4db9d",
+        height: "3rem",
+        borderRadius: "16px 0 16px 0",
+        padding: "16px",
+        margin: "6px",
+        '&:hover': {
+            color: "#f4db9d !important"
+        }
+    }
+}
+
+class Fuser extends Component {
+
+    render(){
+
+        const { classes, fuser } = this.props;
+
+        return(
+            <Paper className={classes.paper}><Typography
+            variant="h6"
+            component={Link}
+            className="foam orange"
+            to={`/${fuser}`}
+          ><strong>{fuser}</strong></Typography></Paper>
+        )
+    }
+}
+
+Fuser.propTypes = {
+    classes: PropTypes.object.isRequired,
+    fuser: PropTypes.string.isRequired
+}
+
+export default withStyles(styles)(Fuser);
