7cf9a1815bb9dcc0d996ec9a3d26fe3c947c777e
rustiphyde <rustiphyde@gmail.com>
2020-02-11 22:58:37 -0600

    fix naming issue with reducer file and add new Type for fetching all requests

--- src/redux/types.js
+++ src/redux/types.js
@@ -37,4 +37,5 @@ export const ADD_TOAST = 'ADD_TOAST';
 export const CHOOZ_BY_LIST = 'CHOOZ_BY_LIST';
 export const SET_INFERNALS = 'SET_INFERNALS';
 export const SET_FUSERS = 'SET_FUSERS';
+export const SET_REQUESTS = 'SET_REQUESTS';
 
