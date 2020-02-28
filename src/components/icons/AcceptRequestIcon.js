cb7abc9097b0c3f99d08de8c5ab0c98bf1adef2a
rustiphyde <rustiphyde@gmail.com>
2020-02-28 00:20:56 -0600

    Add new Accept Request icon

---
+++ src/components/icons/AcceptRequestIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const AcceptRequestIcon = props => (
+    <SvgIcon {...props}>
+        <path d="M21.4,4.8c-1-1-2.8-1.3-4,0l-3,3l-4.9,4.9c-0.2,0.2-0.6,0.2-0.9,0l-2-2c-1.3-1.3-2.8-1.3-4,0c-1,1-1.3,2.8,0,4l4.3,4.5
+	c1,1,2.8,1.3,4,0l3-3l4.5-4.5l3-3C22.6,7.6,22.6,6.1,21.4,4.8z"/>
+    </SvgIcon>
+)
+
+export default AcceptRequestIcon;
