bd94884ae3f80a0158fba8be961434e5aca3f474
rustiphyde <rustiphyde@gmail.com>
2020-03-01 00:30:41 -0600

    Add new icon for RejectButton

---
+++ src/components/icons/RejectRequestIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const RejectRequestIcon = props => (
+    <SvgIcon {...props}>
+        <path d="M11.7,16.3l-3,3c-1.3,1.3-3,1-4.1,0l0,0c-1.3-1.3-1-3,0-4.1l3-3c0,0,0.3-0.3,0-0.5l-3-3c-1.3-1.3-1-3,0-4.1l0,0
+	c1.3-1.3,3-1,4.1,0l3,3c0,0,0.3,0.3,0.5,0l3-3c1.3-1.3,3-1,4.1,0l0,0c1.3,1.3,1.3,2.8,0,4.1l-3,3c0,0-0.3,0.3,0,0.5l3,3
+	c1.3,1.3,1,3,0,4.1l0,0c-1.3,1.3-3,1-4.1,0L11.7,16.3C12.2,16.3,11.7,16.3,11.7,16.3z"/>
+    </SvgIcon>
+)
+
+export default RejectRequestIcon;
