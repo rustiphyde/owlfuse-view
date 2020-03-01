4b20677d8c77faad51fd876dce04ae29427f785f
rustiphyde <rustiphyde@gmail.com>
2020-03-01 00:31:19 -0600

    Add new component for Reject Button

---
+++ src/components/RejectButton.js
@@ -0,0 +1,76 @@
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { rejectFuseRequest } from '../redux/actions/dataActions';
+
+
+import OwlFuseButton from '../util/OwlFuseButton';
+import RejectRequestIcon from './icons/RejectRequestIcon';
+// MUI Stuff
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = {
+
+}
+
+class RejectButton extends Component {
+    state = {
+        open: false
+    }
+    handleOpen = () => {
+        this.setState({ open: true });
+    }
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+    rejectFuseRequest = () => {
+        this.props.rejectFuseRequest(this.props.reqId);
+        this.setState({ open: false });
+    }
+    render(){
+        const { classes } = this.props;
+        return(
+            <Fragment>
+                <OwlFuseButton
+                tip="REJECT FUSE REQUEST"
+                onClick={this.handleOpen}
+                btnClassName={classes.acceptBtn}
+
+                >
+                <RejectRequestIcon className="icon12 orange foam"/>
+                </OwlFuseButton>
+                <Dialog
+                open={this.state.open}
+                onClose={this.handleClose}
+                fullWidth
+                maxWidth="sm"
+                >
+                <DialogTitle variant="h5" className="orng">
+                   Are you sure you wish not to fuse with this user?
+                </DialogTitle>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary" variant="contained">
+                        CANCEL
+                    </Button>
+                    <Button onClick={this.rejectFuseRequest} color="primary" variant="contained">
+                        REJECT
+                    </Button>
+                </DialogActions>
+                </Dialog>
+            </Fragment>
+        )
+    }
+}
+
+RejectButton.propTypes = {
+    reqId: PropTypes.string.isRequired,
+    rejectFuseRequest: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired
+}
+
+export default connect(null, { rejectFuseRequest })(withStyles(styles)(RejectButton));
