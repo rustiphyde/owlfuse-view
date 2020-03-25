26b224db20c30ac4ba5eb8a5bf60c3c155afae95
rustiphyde <rustiphyde@gmail.com>
2020-03-25 01:06:19 -0500

    Start new Fuse Panel Component

---
+++ src/components/FusePanel.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import OwlFuseButton from '../util/OwlFuseButton';
+
+import OwlFuseLogo from './icons/OwlFuseLogo';
+
+const styles = {
+
+}
+
+class FusePanel extends Component {
+    render(){
+        return(<OwlFuseButton
+           tip="OPEN FUSE PANEL">
+            <OwlFuseLogo className="foam orange icon13"/>
+        </OwlFuseButton>);
+    }
+}
+
+export default withStyles(styles)(FusePanel);
