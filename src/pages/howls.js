32c822e6bae096927f4bc85493faa87f90de078f
rustiphyde <rustiphyde@gmail.com>
2020-03-26 23:46:10 -0500

    Start Howls page for viewing and interacting with current user's howls

---
+++ src/pages/howls.js
@@ -0,0 +1,21 @@
+import React, {Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+//MUI Stuff
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+
+}
+
+class howls extends Component{
+    render(){
+
+        return(<></>)
+    }
+}
+
+
+
+
+export default withStyles(styles)(howls);
