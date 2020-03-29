3e9439472ef9810b88d46c9558c8590e3e751426
rustiphyde <rustiphyde@gmail.com>
2020-03-29 01:24:20 -0500

    Add Clock Component to Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import HowlDash from './howls/HowlDash';
+import OwlClock from './OwlClock';
 
 export class Footer extends Component {
   render() {
       return (
-
-          <div className="footer">
+        <Fragment>
+           <div className="footer">
+            
+           <HowlDash/>
+           
           <hr className="bar-separator"/>
-          <p><strong>© {new Date().getFullYear()} Rusty Hoppins</strong></p>
-          <hr className="bar-separator"/>
+          <p className="footer-text">© {new Date().getFullYear()} Rusty Hoppins, All Rights Reserved.</p>
+          <span><OwlClock/></span>
+          <hr className="bar-separator"/>  
         </div>
+        
+        </Fragment>
+         
         
       )
   }
