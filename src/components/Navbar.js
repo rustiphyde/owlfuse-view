0756e034a20e628f911b2e9794386146a679c332
rustiphyde <rustiphyde@gmail.com>
2020-04-05 00:35:21 -0500

    Add toggle for displaying other menu in NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { withStyles } from '@material-ui/core/styles'
 import PropTypes from "prop-types";
 import OwlFuseButton from "../util/OwlFuseButton";
 import OwlFuseTitle from "../images/owlfuse-title.png";
 
 // Components
 import PostSpark from './sparks/PostSpark';
-import SizzlesAndClinks from './SizzlesAndClinks'
+import SizzlesAndClinks from './SizzlesAndClinks';
+import Toggle from './Toggle';
 
 // MUI Components
 import AppBar from "@material-ui/core/AppBar";
@@ -18,10 +20,34 @@ import Button from "@material-ui/core/Button";
 import HomeIcon from "./icons/HomeIcon";
 import OkelistIcon from "./icons/OkelistIcon";
 import BoozulaIcon from './icons/BoozulaIcon';
+import HowlIcon from './icons/HowlIcon';
+
+const styles = {
+  toggleIsActive: {
+    color: '#ff9800 !important'
+  },
+  toggleIsInactive: {
+    color: '#f4db9d !important'
+  }
+}
 
 export class Navbar extends Component {
+  state = {
+    toggleChecked: false
+  }
+
+  handleToggle = event => {
+		if (event.target.checked) {
+			this.setState({ toggleChecked: true });
+			console.log("toggle is checked");
+		} else {
+			this.setState({ toggleChecked: false });
+			console.log("toggle is not checked");
+		}
+	};
+
   render() {
-    const { authenticated } = this.props;
+    const { authenticated, classes } = this.props;
     return (
       <AppBar>
         <Fragment>
@@ -31,10 +57,19 @@ export class Navbar extends Component {
             className="nav-logo"
           />
           <hr className="bar-separator" />
+          {authenticated ? (
+            <Fragment>
+              <div className="centered">
+						<span className={`toggle-text ${!this.state.toggleChecked ? classes.toggleIsActive : classes.toggleIsInactive }`}>Owl</span>
+						<Toggle toggleFunx={this.handleToggle} />
+						<span className={`toggle-text ${this.state.toggleChecked ? classes.toggleIsActive : classes.toggleIsInactive }`}>Fuse</span>
+					</div></Fragment>
+          ) : null }
         </Fragment>
         <Toolbar className="nav-container">
           {authenticated ? (
-            <Fragment>
+            !this.state.toggleChecked ? (
+              <Fragment>
               <PostSpark />
               <Link to="/boozulas">
                 <OwlFuseButton tip="BOOZULAS">
@@ -53,6 +88,25 @@ export class Navbar extends Component {
               </Link>              
                 <SizzlesAndClinks />
             </Fragment>
+            ) : (
+              <Fragment>
+              <PostSpark />              
+                <OwlFuseButton tip="HOWL MENU">
+                  <HowlIcon />
+                </OwlFuseButton>
+              <Link to="/">
+                <OwlFuseButton tip="GO HOME">
+                  <HomeIcon />
+                </OwlFuseButton>
+              </Link>
+              <Link to="/okelists">
+                <OwlFuseButton tip="OKELISTS">
+                  <OkelistIcon />
+                </OwlFuseButton>
+              </Link>              
+                <SizzlesAndClinks />
+            </Fragment>
+            )
           ) : (
             <Fragment>
               <Button color="inherit" component={Link} to="/login">
@@ -73,11 +127,12 @@ export class Navbar extends Component {
 }
 
 Navbar.propTypes = {
-  authenticated: PropTypes.bool.isRequired
+  authenticated: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
   authenticated: state.user.authenticated
 });
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps)(withStyles(styles)(Navbar));
