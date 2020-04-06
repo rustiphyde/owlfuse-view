a19ff11e31d6a5b57b26bc841e88f0f25663b516
rustiphyde <rustiphyde@gmail.com>
2020-04-06 01:59:30 -0500

    Flesh out howls page some more

--- src/pages/howls.js
+++ src/pages/howls.js
@@ -1,76 +1,91 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
-import { fetchUserHowls } from '../redux/actions/dataActions';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import { fetchUserHowls } from "../redux/actions/dataActions";
 
 // MUI Stuff
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 //Icons
-import HowlIcon from '../components/icons/HowlIcon';
+import HowlIcon from "../components/icons/HowlIcon";
 
 const styles = {
-    titleBar: {
-        display: 'flex',
-        backgroundColor: '#263238',
-        color: '#ff9800',
-        paddingLeft: '3rem',
+	titleBar: {
+		display: "flex",
+		backgroundColor: "#263238",
+		color: "#ff9800",
+		paddingLeft: "3rem",
+		width: "100%",
+	},
+	back: {
+		marginTop: "-3.5rem",
+        backgroundColor: "#263238",
         width: '100%'
-    },
-    back: {
-        marginTop: '-3.5rem',
-        backgroundColor: '#263238'
-    }
-}
+	},
+};
 
 class howls extends Component {
-    state = {
-        howls: null
-    }
+	state = {
+		howls: null,
+	};
 
-    componentDidMount(){
-        this.props.fetchUserHowls();
-    }
+	componentDidMount() {
+		this.props.fetchUserHowls();
+	}
 
-    render(){
-        const { classes } = this.props;
-        const { howls } = this.props.data;
+	render() {
+		const { classes } = this.props;
+		const { howls } = this.props.data;
 
-        return(<Grid container spacing={1} className={classes.back}>
-            <Grid item sm={12}>
-                <Fragment>
-                    <hr className="bar-separator"/>
-                    <div className="centered">
-                    <span className={classes.titleBar}><HowlIcon className="icon5"/>
-                    <Typography variant="h3" lassName={classes.title}>
-                        <strong c>HOWLBOX</strong>
-                    </Typography>
-                    </span>
-                    </div>
-                    
-                </Fragment>
-            </Grid>
-
-        </Grid>)
-    }
+		return (
+			<Grid container spacing={1}>
+				<Grid item sm={12} className={classes.back}>
+					<Fragment>
+						<hr className="bar-separator" />
+						<div className="centered">
+							<span className={classes.titleBar}>
+								<HowlIcon className="icon5" />
+								<Typography variant="h3" lassName={classes.title}>
+									<strong c>HOWLBOX</strong>
+								</Typography>
+							</span>
+						</div>
+					</Fragment>
+				</Grid>
+				<Grid item sm={4}>
+					<Fragment>
+						<div>Hello</div>
+					</Fragment>
+				</Grid>
+				<Grid item sm={8}>
+					<Fragment>
+                        <div>Howls Go Here</div>
+                    </Fragment>
+				</Grid>
+			</Grid>
+		);
+	}
 }
 
 howls.propTypes = {
-    classes: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired,
-    fetchUserHowls: PropTypes.func.isRequired
-}
+	classes: PropTypes.object.isRequired,
+	data: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
+	fetchUserHowls: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
-    user: state.user,
-    data: state.data
+const mapStateToProps = (state) => ({
+	user: state.user,
+	data: state.data,
 });
 
 const mapActionsToProps = {
-    fetchUserHowls
-}
+	fetchUserHowls,
+};
 
-export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(howls));
+export default connect(
+	mapStateToProps,
+	mapActionsToProps
+)(withStyles(styles)(howls));
