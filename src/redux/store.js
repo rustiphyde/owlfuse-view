4a9f248b9be1e002c22e02963e0ed63e8782973d
rustiphyde <rustiphyde@gmail.com>
2020-05-10 22:51:36 -0500

    Add redux-firebase and firestore reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
 // store file "stores" the application state
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-
+import { config } from '../util/config';
+import { firestoreReducer, getFirestore, reduxFirestore } from 'redux-firestore';
+import { firebaseReducer, getFirebase } from 'react-redux-firebase';
 import userReducer from "./reducers/userReducer";
 import dataReducer from "./reducers/dataReducer";
 import uiReducer from "./reducers/uiReducer";
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [thunk.withExtraArgument({ getFirestore, getFirebase })];
 
 // Actual state 
 const reducers = combineReducers({
@@ -17,7 +19,11 @@ const reducers = combineReducers({
   // Everything that comes from dataReducer.js will be stored inside the data object
   data: dataReducer,
   // Everything that comes from uiReducer.js will be stored inside the UI object
-  UI: uiReducer
+  UI: uiReducer,
+  // Everthing that comes from firebaseReducer will be stored inside the firebase object
+  firebase: firebaseReducer,
+  // Everthing that comes from firestoreReducer will be stored inside the firestore object
+  firestore: firestoreReducer
 });
 
 const composeEnhancers =
@@ -25,7 +31,7 @@ const composeEnhancers =
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const enhancer = composeEnhancers(applyMiddleware(...middleware), reduxFirestore(config));
 const store = createStore(reducers, initialState, enhancer);
 
 export default store;
