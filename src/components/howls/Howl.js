e05994111efd8a492484f11bd76593d9a347a8d6
rustiphyde <rustiphyde@gmail.com>
2020-05-15 21:00:28 -0500

    Convert to Functional Component using react-redux-firebase hooks

--- src/components/howls/Howl.js
+++ src/components/howls/Howl.js
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import OwlFuseButton from "../../util/OwlFuseButton";
 import { connect, useSelector } from "react-redux";
-import { compose } from 'redux';
-import firebase from 'firebase/app';
-import 'firebase/firestore'
-import { useFirestoreConnect } from 'react-redux-firebase';
+import { compose } from "redux";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useFirestoreConnect, useFirebaseConnect } from "react-redux-firebase";
 import { Avatar, CircularProgress } from "@material-ui/core";
-import EditHowl from './EditHowl';
+import EditHowl from "./EditHowl";
 
 const styles = {
 	userSent: {
@@ -17,23 +17,23 @@ const styles = {
 		color: "#f4db9d",
 		width: "75%",
 		float: "right",
-		lineHeight: '20px',
-		letterSpacing: '1px',
+		lineHeight: "20px",
+		letterSpacing: "1px",
 		margin: "8px",
 		wordWrap: "break-word",
 		fontWeight: 700,
 		borderRadius: "16px 0 16px 0",
-    },
-    fuserSentPic: {
-        float: 'left',
-        margin: '6px 16px',
-        border: '1.5px solid #f4db9d'
-    },
-    	fuserSent: {
+	},
+	fuserSentPic: {
+		float: "left",
+		margin: "6px 16px",
+		border: "1.5px solid #f4db9d",
+	},
+	fuserSent: {
 		backgroundColor: "#ff9800",
 		padding: "16px",
-		letterSpacing: '1px',
-		lineHeight: '20px',
+		letterSpacing: "1px",
+		lineHeight: "20px",
 		color: "#263238",
 		width: "75%",
 		float: "left",
@@ -50,100 +50,109 @@ const styles = {
 	},
 };
 
-const Howl = (props) => {
 
-	useFirestoreConnect({collection: 'Howls', orderBy: ["createdAt", "asc"]});
-	const howls = useSelector(state => state.firestore.ordered.Howls);
+
+const Howl = (props) => {
+	useFirestoreConnect({ collection: "Howls", orderBy: ["createdAt", "asc"] });
+	const howls = useSelector((state) => state.firestore.ordered.Howls);
 
 	useEffect(() => {
 		
 		setTimeout(() => {
+
 			let cont = document.getElementById("howl-container");
-			if(cont){
+			if (cont) {
 				cont.scrollTo(0, cont.scrollHeight);
-				}
+			}
 		}, 3000);
-	},
-	[]);
+	}, []);
 
 	useEffect(() => {
 		let cont = document.getElementById("howl-container");
-			
-		if(cont){
-		cont.scrollTo(0, cont.scrollHeight);
+
+		if (cont) {
+			cont.scrollTo(0, cont.scrollHeight);
 		}
-	},
-	[howls]);
-
-
-    
-		let howlingsMarkup = !props.loading ? (
-			howls && howls.length > 0 ? (
-				howls.map((howl) => {
-				if(howl.docKey === [props.fuser, props.clozang].sort().join("::")){
+	}, [howls]);
+	let howlText = "";
+	let howlingsMarkup = !props.loading ? (
+		howls && howls.length > 0 ? (
+			
+			howls.map((howl) => {
+				if (howl.docKey === [props.fuser, props.clozang].sort().join("::")) {
 					let index = howls.indexOf(howl);
+					howlText = "";
 					return (
 						<div key={index + "54"}>
-							
 							<div
 								key={index}
 								className={
-									howl.sentBy === props.clozang ? props.classes.userSent : props.classes.fuserSent
+									howl.sentBy === props.clozang
+										? props.classes.userSent
+										: props.classes.fuserSent
 								}
 							>
-								
-                               {
-								   howl.sentBy === props.clozang ? (<EditHowl howl={howl} howlId={howl.howlId} className={props.classes.userSentPic} />) : <Avatar key={index + "83"} src={howl.avatar} className={
-									props.classes.fuserSentPic
-								}>
-
-</Avatar>
-							   }
+								{howl.sentBy === props.clozang ? (
+									<EditHowl
+										howl={howl}
+										howlId={howl.howlId}
+										className={props.classes.userSentPic}
+									/>
+								) : (
+									<Avatar
+										key={index + "83"}
+										src={howl.avatar}
+										className={props.classes.fuserSentPic}
+									></Avatar>
+								)}
 								{howl.howlBody}
 							</div>
 						</div>
 					);
-							}
-				})
-			) : (
-				<strong className="candle centered">
-					You do not have any howls to view at this time
-				</strong>
-			)
+				}
+				else {
+					howlText = "Sorry, but you do not have any howls to view at this time."
+				}
+			})
 		) : (
-			<CircularProgress
-            color="secondary"
-            size={80}
-            className="candle centered"
-        />
-		);
+			<strong className="candle centered">
+				You do not have any howls to view at this time
+			</strong>
+		)
+	) : (
+		<CircularProgress color="secondary" size={80} className="candle centered" />
+	);
 
-		return (
-			<div>
-				<div id="howl-container" className={props.classes.content}>
-					{howlingsMarkup}
-				</div>
+	return (
+		<div>
+			<div id="howl-container" className={props.classes.content}>
+				<strong className="candle centered">{howlText}</strong>
+				{howlingsMarkup}
 			</div>
-		);
-}
+		</div>
+	);
+};
 
 Howl.propTypes = {
 	classes: PropTypes.object.isRequired,
 	howls: PropTypes.array,
 	data: PropTypes.object.isRequired,
 	user: PropTypes.object.isRequired,
-	fuser: PropTypes.string
+	fuser: PropTypes.string,
 };
 
-const mapStateToProps = (state) => {	
-return {	
-	data: state.data,
-	user: state.user,
-	fuser: state.data.fuser.fuser,
-	clozang: state.user.credentials.clozang,
-	loading: state.UI.loading
-
-	}
+const mapStateToProps = (state) => {
+	return {
+		data: state.data,
+		user: state.user,
+		fuser: state.data.fuser.fuser,
+		clozang: state.user.credentials.clozang,
+		loading: state.UI.loading,
+		auth: state.firebase.auth,
+		profile: state.firebase.profile
+	};
 };
 
-export default compose(connect(mapStateToProps, null))(withStyles(styles)(Howl));
+export default compose(connect(mapStateToProps, null))(
+	withStyles(styles)(Howl)
+);
