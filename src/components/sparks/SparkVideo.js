3535f3f5c2b456145791f1b73c3df47ef472220f
Rusty Hoppins <rusty@beanstalkwebsolutions.com>
2020-08-16 22:46:43 -0500

    Update spark video component to more mirror the spark Image functionality

--- src/components/sparks/SparkVideo.js
+++ src/components/sparks/SparkVideo.js
@@ -1,63 +1,166 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import OwlFuseButton from '../../util/OwlFuseButton';
-import { withStyles } from '@material-ui/core/styles';
-import AddVideoIcon from '../icons/AddVideoIcon';
-import { addSparkVideo } from '../../redux/actions/dataActions';
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import OwlFuseButton from "../../util/OwlFuseButton";
+import CloseIcon from "../icons/CloseIcon";
+import { withStyles } from "@material-ui/core/styles";
+import AddVideoIcon from "../icons/AddVideoIcon";
+import { addSparkVideo } from "../../redux/actions/dataActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { Typography } from "@material-ui/core";
+import { POST_SPARK_VIDEO } from "../../redux/types";
 
-const styles = {
+const styles = (theme) => ({
+	...theme.themeMinusPalette,
+	submitButton: {
+		position: "relative",
+		margin: "10px auto",
+	},
+	progressSpinner: {
+		position: "absolute",
+	},
+	closeButton: {
+		position: "absolute",
+		left: "80%",
+		top: "4%",
+		color: "#f4db9d",
+	},
+	textField: {
+		textAlign: "center",
+	},
+});
 
-}
+class SparkVideo extends Component {
+	state = {
+		open: false,
+		body: "",
+	};
 
+	formData = new FormData();
 
-class SparkVideo extends Component{
+	openVideoSpark = () => {
+		this.setState({ open: true });
+	};
+	handleClose = () => {
+		this.setState({ open: false });
+	};
 
-    handleVideoAdd = event => {
-        const video = event.target.files[0];
-        const formData = new FormData();
-        formData.append("video", video, video.name);
-        this.props.addSparkVideo(formData);
-      };
-      handleAddVideo = () => {
-        const fileInput = document.getElementById("sparkVideoInput");
-        fileInput.click();
-      };
+	handleVideoAdd = (event) => {
+		const video = event.target.files[0];
+		this.formData.append("video", video, video.name);
+	};
+	handleAddVideo = () => {
+		const fileInput = document.getElementById("sparkVideoInput");
+		fileInput.click();
+	};
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	handlePress = () => {
+		const buttonPress = document.getElementById('postButton');
+		buttonPress.click();
+	}
+	handleSubmit = (event) => {
+		event.preventDefault();
+		this.formData.append("body", this.state.body);
+		this.props.addSparkVideo(this.formData);
+		this.handleClose();
+		this.props.closeFunx();
+	};
 
-    render(){
+	render() {
+		const { classes,  UI: { loading } } = this.props;
 
-        const { classes } = this.props;
-
-        return(
-           <Fragment>
-               <input
-                type="file"
-                id="sparkVideoInput"
-                hidden="hidden"
-                onChange={this.handleVideoAdd}
+		return (
+			<Fragment>
+				<OwlFuseButton tip="IMAGE SPARK" onClick={this.openVideoSpark}>
+					<AddVideoIcon className="oaky orange icon5" />
+				</OwlFuseButton>
+				<Dialog
+					open={this.state.open}
+					onClose={this.handleClose}
+					fullWidth
+					maxWidth="sm"
+				>
+					<OwlFuseButton
+						tip="CLOSE"
+						onClick={this.handleClose}
+						tipClassName={classes.closeButton}
+					><CloseIcon className="orange"/>
+                    </OwlFuseButton>
+                    <DialogTitle variant="h5" className="orng">POST A VIDEO SPARK</DialogTitle>
+                    <DialogContent className="orange-border">
+					<form onSubmit={this.handleSubmit}>
+                    <input
+						type="file"
+						id="sparkVideoInput"
+						hidden="hidden"
+						onChange={this.handleVideoAdd}
+					/>
+                    <TextField
+                name="body"
+                type="text"
+                label="SPARK AN INTEREST"
+                multiline
+                rows="3"
+                placeholder="SPARK IT UP"
+                className={`${this.props.classes.textField} ${this.props.classes.displayLinebreaks}`}
+                onChange={this.handleChange}
+                fullWidth
               />
-               <OwlFuseButton
-               tip="VIDEO SPARK"
-               onClick={this.handleAddVideo}>
-                   <AddVideoIcon className="oaky orange icon5"/>
-               </OwlFuseButton>
-           </Fragment>
-        )
-    }
+			  <hr/>
+			  <strong className="candle centered rusty">CHOOSE YOUR VIDEO</strong>
+              <OwlFuseButton tip="VIDEO SPARK" onClick={this.handleAddVideo}>
+						<AddVideoIcon className="oaky orange icon5" />
+					</OwlFuseButton>
+              <button
+				type="submit"
+                variant="contained"
+                color="primary"
+				disabled={loading}
+				id="postButton"
+				hidden="hidden"
+              ></button>
+                    </form>
+                    <DialogActions>
+					<Button onClick={this.handlePress} color="primary" variant="contained">
+              <strong className="orange">SUBMIT</strong>
+            </Button>
+          <Button onClick={this.handleClose} color="primary" variant="contained">
+              <strong className="orange">CANCEL</strong>
+            </Button>
+          </DialogActions>
+                    </DialogContent>
+				</Dialog>
+			</Fragment>
+		);
+	}
 }
 
 SparkVideo.propTypes = {
-    addSparkVideo: PropTypes.func.isRequired
-}
+    addSparkVideo: PropTypes.func.isRequired,
+	UI: PropTypes.object.isRequired,
+	closeFunx: PropTypes.func.isRequired,
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     user: state.user,
-    data: state.data,
-    auth: state.firebase.auth
+    UI: state.UI,
+	data: state.data,
+	auth: state.firebase.auth,
 });
 
 const mapActionsToProps = {
-    addSparkVideo
-}
+	addSparkVideo,
+};
 
-export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(SparkVideo));
+export default connect(
+	mapStateToProps,
+	mapActionsToProps
+)(withStyles(styles)(SparkVideo));
