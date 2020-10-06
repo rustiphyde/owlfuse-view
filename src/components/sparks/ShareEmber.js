2644fa6af11da786208c4f159c87e9e0786bf414
Rusty Hoppins <rusty@beanstalkwebsolutions.com>
2020-10-05 23:21:34 -0500

    Add functionality to Share Ember component

--- src/components/sparks/ShareEmber.js
+++ src/components/sparks/ShareEmber.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { getSpark, shareEmber } from '../../redux/actions/dataActions';
+import PropTypes from "prop-types";
+import dayjs from "dayjs";
+import { connect } from "react-redux";
+import { getSpark, shareEmber, getSparks } from "../../redux/actions/dataActions";
 import { withStyles } from "@material-ui/core/styles";
 import OwlFuseButton from "../../util/OwlFuseButton";
 import {
@@ -14,40 +15,206 @@ import {
 	Dialog,
 	Paper,
 	Typography,
-	Grid
+	Grid,
 } from "@material-ui/core";
-import EmberIcon from '../icons/EmberIcon';
+import EmberIcon from "../icons/EmberIcon";
+import CloseIcon from "../icons/CloseIcon";
 
-const styles = {
-
-}
+const styles = (theme) => ({
+	...theme.themeMinusPalette,
+	textField: {
+		textAlign: "center",
+    },
+    halfHeight: {
+        height: 'auto',
+        width: '100%'
+    },
+	dialogContent: {
+		padding: 20,
+		color: "#ff9800",
+		overflowY: "auto",
+		overflowX: "hidden",
+		backgroundColor: "primary",
+	},
+	closeButton: {
+		position: "absolute",
+		left: "82%",
+		top: "2%",
+	},
+	submitButton: {
+		position: "relative",
+		margin: "10px auto",
+	},
+	progressSpinner: {
+		position: "absolute",
+	},
+	spimg: {
+		width: "100%",
+		objectFit: "cover",
+		marginTop: "16px",
+    },
+    shareForm: {
+        textAlign: 'left !important'
+    }
+});
 
 class ShareEmber extends Component {
+	state = {
+		open: false,
+		body: "",
+	};
 
-    state = {
-        open: false,
-        body: "",
-    }
+	componentDidMount() {
+		this.props.getSpark(this.props.emberId);
+	}
 
-    render(){
+	handleOpen = () => {
+		this.setState({ open: true });
+	};
 
-        const { classes } = this.props;
+	handleClose = () => {
+		this.setState({ open: false });
+	};
 
-        return(
-            <div></div>
-        )
-    }
+	handleChange = (event) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+        this.props.shareEmber(this.props.emberId, { body: this.state.body });
+        setTimeout(this.props.getSparks, 5000);
+        this.handleClose();
+
+	};
+
+	render() {
+		const {
+			classes,
+			spark: {
+				userClozang,
+				body,
+				createdAt,
+				sparkImage,
+				sparkVideo,
+				sparkAudio,
+				sparkLink,
+			},
+			UI: { loading },
+		} = this.props;
+
+		let emberContent = sparkImage ? (
+			<Fragment>
+				<img src={sparkImage} alt="spark image" className={classes.spimg} />
+				<br />
+			</Fragment>
+		) : sparkVideo ? (
+			<Fragment>
+				<div className="centered">
+					<iframe src={sparkVideo} allowFullScreen></iframe>
+				</div>
+				<br />
+			</Fragment>
+		) : sparkAudio ? (
+			<Fragment>
+				<hr className="bar-separator" />
+				<div id="aPlayer" className="centered">
+					<audio
+						controls
+						style={{ backgroundColor: "transparent", outline: "none" }}
+					>
+						<source
+							style={{
+								backgroundColor: "#ff9800",
+								borderRadius: "16px 0 16px 0",
+							}}
+							src={sparkAudio}
+						></source>
+					</audio>
+				</div>
+				<hr className="bar-separator" />
+			</Fragment>
+		) : null;
+
+		return (
+			<Fragment>
+				<OwlFuseButton tip="SHARE AN EMBER" onClick={this.handleOpen}>
+					<EmberIcon color="primary" className="icon6 orange" />
+				</OwlFuseButton>
+				<Dialog open={this.state.open} onClose={this.handleClose} className={classes.halfHeight}>
+					<OwlFuseButton
+						tip="CLOSE"
+						onClick={this.handleClose}
+						tipClassName={classes.closeButton}
+					>
+						<CloseIcon className="orange" />
+					</OwlFuseButton>
+					<DialogTitle variant="h5" className="orng">
+						SHARE AN EMBER
+					</DialogTitle>
+					<DialogContent className="orange-border">
+						<form onSubmit={this.handleSubmit}>
+							<TextField
+								name="body"
+								type="text"
+								label="ADD TEXT"
+								multiline
+								rows="2"
+								placeholder="SPARK IT"
+								className={`${this.props.classes.textField} ${this.props.classes.displayLinebreaks}`}
+								onChange={this.handleChange}
+								fullWidth
+							/>
+							<hr className="bar-separator" />
+							<span className={classes.shareForm}>
+								<Typography variant="body2" color="primary">
+									<strong>{userClozang} </strong>
+								</Typography>
+								<Typography variant="body2" color="textSecondary">
+									{dayjs(createdAt).format("h:mm a, MMMM D, YYYY")}
+								</Typography>
+							<Typography variant="body2" color="primary" className="breaks">
+								<b>{body}</b>
+							</Typography>
+                            </span>
+                            {emberContent}
+                            <hr className="bar-separator" />
+							<Button
+								type="submit"
+								variant="contained"
+								color="primary"
+								className={classes.submitButton}
+								disabled={loading}
+							>
+								SHARE
+								{loading && (
+									<CircularProgress
+										size={30}
+										className={classes.progressSpinner}
+									/>
+								)}
+							</Button>
+						</form>
+					</DialogContent>
+				</Dialog>
+			</Fragment>
+		);
+	}
 }
 
 ShareEmber.propTypes = {
-    shareEmber: PropTypes.func.isRequired,
-    classes: PropTypes.object.isRequired,
-    getSpark: PropTypes.func.isRequired,
-    emberId: PropTypes.string.isRequired,
-}
+	shareEmber: PropTypes.func.isRequired,
+	classes: PropTypes.object.isRequired,
+	getSpark: PropTypes.func.isRequired,
+	emberId: PropTypes.string.isRequired,
+    spark: PropTypes.object.isRequired,
+    getSparks: PropTypes.func.isRequired
+};
 
-const mapStateToProps = state => ({
-    UI: state.UI
-})
+const mapStateToProps = (state) => ({
+	UI: state.UI,
+});
 
-export default connect(mapStateToProps, { getSpark, shareEmber })(withStyles(styles)(ShareEmber));
+export default connect(mapStateToProps, { getSpark, shareEmber, getSparks })(
+	withStyles(styles)(ShareEmber)
+);
